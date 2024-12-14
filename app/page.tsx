0f005dee197ec049ea0f68f240eca3f6dab1e36a
Binod Chaudhary <thebinod7@gmail.com>
2024-12-14 20:52:44 +0545

    realign components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-import MyApp from "./components/MyApp";
+"use client";
+
+import React from "react";
+import { SCREENS } from "./contants";
+import Welcome from "./components/Welcome";
+import ConfirmEmail from "./components/ConfirmEmail";
+import GetStarted from "./components/GetStarted";
 
 export default function Home() {
-  return <MyApp />;
+  const [screen, setScreen] = React.useState(SCREENS.WELCOME);
+
+  const handleScreenChange = (screen: string) => {
+    setScreen(screen);
+  };
+
+  return (
+    <div className="center-container">
+      {screen === SCREENS.WELCOME && (
+        <Welcome handleScreenChange={handleScreenChange} />
+      )}
+      {screen === SCREENS.CONFIRM_EMAIL && (
+        <ConfirmEmail handleScreenChange={handleScreenChange} />
+      )}
+      {screen === SCREENS.GET_STARTED && (
+        <GetStarted handleScreenChange={handleScreenChange} />
+      )}
+    </div>
+  );
 }
